0e5a4140eb05df7360b0f9fefeaf918ee68b9f23
Anshu05032008 <anshucwd@gmail.com>
2026-08-04 14:33:01 +0530

    DSA

---
+++ Array_trevarsing.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int arr[5]={10,20,30,40,50};
+       printf("Enter array elements");
+       for(int i=0;i<5;i++)
+        {
+         
+         printf("\nElement=%d",arr[i]);
+          printf("--Index_place=%d",i);
+        }
+return 0;
+}
